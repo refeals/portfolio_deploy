929e880fbc8605f1b775951e5056e7c611895633
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2021-01-13 11:31:03 -0300

    Add Navbar and about pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps /* , AppContext */ } from 'next/app'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,25 +1,26 @@
-import { init, send } from 'emailjs-com'
-import { useEffect } from 'react'
+import { send } from 'emailjs-com'
 import { useForm } from 'react-hook-form'
 
 const Contact = () => {
   const { register, handleSubmit, errors } = useForm()
 
-  useEffect(() => {
-    init(process.env.NEXT_PUBLIC_EMAIL_JS_USER_ID)
-  }, [])
-
-  const sendFeedback = (serviceID, templateId, variables) => {
-    send(serviceID, templateId, variables)
-      .then(res => console.log('Email successfully sent!'))
-      .catch(err => console.error(err))
-  }
-
   const onSubmit = (data, r) => {
     const templateId = process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE_ID
-    const serviceID = process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID
-    sendFeedback(serviceID, templateId, { from_name: data.name, message_html: data.comment, reply_to: data.email })
-    r.target.reset()
+    const serviceId = process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID
+    const userId = process.env.NEXT_PUBLIC_EMAIL_JS_USER_ID
+    const variables = {
+      from_name: data.name,
+      message_html: data.comment,
+      reply_to: data.email,
+      from_email: data.email
+    }
+
+    send(serviceId, templateId, variables, userId)
+      .then((res) => {
+        console.log('Email successfully sent!')
+        r.target.reset()
+      })
+      .catch((err) => console.error(err))
   }
 
   return (
@@ -28,15 +29,13 @@ const Contact = () => {
         <input
           placeholder="name"
           name="name"
-          ref={
-            register({
-              required: 'Please enter your name',
-              maxLength: {
-                value: 20,
-                message: 'Please enter a name with fewer than 20 characters',
-              },
-            })
-          }
+          ref={register({
+            required: 'Please enter your name',
+            maxLength: {
+              value: 20,
+              message: 'Please enter a name with fewer than 20 characters'
+            }
+          })}
         />
         <br />
         {errors.name && errors.name.message}
@@ -45,15 +44,13 @@ const Contact = () => {
         <input
           placeholder="email"
           name="email"
-          ref={
-            register({
-              required: 'Please enter an email',
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'invalid email address',
-              },
-            })
-          }
+          ref={register({
+            required: 'Please enter an email',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'invalid email address'
+            }
+          })}
         />
         <br />
         {errors.email && errors.email.message}
@@ -62,11 +59,9 @@ const Contact = () => {
         <textarea
           placeholder="comment"
           name="comment"
-          ref={
-            register({
-              required: true,
-            })
-          }
+          ref={register({
+            required: true
+          })}
         />
         <br />
         {errors.comment && 'oops, you forgot your message!'}
